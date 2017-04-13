dd35921a985dfa740c0f448ede1996d0af9b0c96
Bagchi <nbagchi@tbsc02s60qng8wm.turner.com>
2017-04-13 15:25:22 -0400

    inital commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express=require('express');
+const hbs=require('hbs');
+var app=express();
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrentDate',()=>{
+    return new Date().getFullYear();
+})
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+app.get('/',(req,res)=>{
+res.render('home.hbs',{name:'Nochiketa Bagchi',
+personType:'old man',
+pageTitle:'Home Page',
+currentYear:'2017'
+});
+});
+app.get('/bad',(req,res)=>{
+res.send({errorMsg:'Bad Request'});
+});
+app.get('/about',(req,res)=>{
+res.render('about.hbs',{
+    pageTitle:'About Page',
+    currentYear:'2017'
+})
+});
+app.listen(3000);
